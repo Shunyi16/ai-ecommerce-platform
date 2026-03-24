c7687ec66d2fa61558802393551b9b7b226305c6
Shunyi Liu <shunyiliu@Shunyis-MacBook-Pro.local>
2026-03-24 16:23:42 -0600

    created routes for read, update and delete a product.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import SQLModel, Session, select
 from database import engine
 from models import (
@@ -6,7 +6,7 @@ from models import (
     Product,
     Order,
     OrderItem,
-)  # Imports your tables so SQLModel knows they exist
+) 
 
 app = FastAPI()
 
@@ -21,7 +21,7 @@ def on_startup():
 def read_root():
     return {"message": "Hello from FastAPI! SQLModel is officially running."}
 
-
+# Create an user account
 @app.post("/users")
 def create_user(user: User):
     # Open a connection
@@ -31,7 +31,7 @@ def create_user(user: User):
         session.refresh(user)
         return user
 
-
+# Read an user account
 @app.get("/users")
 def read_users():
     with Session(engine) as session:
@@ -39,7 +39,7 @@ def read_users():
         users = session.exec(statement).all()
         return users
 
-
+# Create a product
 @app.post("/products")
 def create_product(product: Product):
     # Open a connection
@@ -48,16 +48,53 @@ def create_product(product: Product):
         session.commit()
         session.refresh(product)
         return product
+    
+# Update a product
+@app.patch("/products/{product_id}")
+def update_product(product_id: int, product_update:Product):
+    # Open a connection
+    with Session(engine) as session:
+        db_product = session.get(Product, product_id)
+        if not db_product:
+            raise HTTPException(status_code=404, detail="Product not found")
+        
+        update_data = product_update.model_dump(exclude_unset=True)        
+        db_product.sqlmodel_update(update_data)
+        session.add(db_product)
+        session.commit()
+        session.refresh(db_product)
+        return db_product
+    
+# Delete a product
+@app.delete("/products/{product_id}")
+def delete_product(product_id:int):
+    with Session(engine) as session:
+        product_delete = session.get(Product, product_id)
+        if not product_delete:
+            raise HTTPException(status_code=404, detail="Product not found")
+        session.delete(product_delete)
+        session.commit()
+        return {"message" : f"Product {product_id} deleted successfully"}
 
-
+# Read products
 @app.get("/products")
 def read_products():
     with Session(engine) as session:
         statement = select(Product)
         products = session.exec(statement).all()
         return products
+    
+# Read a product
+@app.get("/products/{product_id}")
+def read_a_product(product_id: int):
+    with Session(engine) as session:
+        product = session.get(Product, product_id)
 
+        if not product:
+            raise HTTPException(status_code=404, detail="Product not found")
+        return product
 
+# Create an order
 @app.post("/orders")
 def create_order(order: Order):
     # Open a connection
@@ -67,7 +104,7 @@ def create_order(order: Order):
         session.refresh(order)
         return order
 
-
+# Read orders
 @app.get("/orders")
 def read_orders():
     with Session(engine) as session:
@@ -75,7 +112,7 @@ def read_orders():
         orders = session.exec(statement).all()
         return orders
 
-
+# Create items of the order
 @app.post("/items")
 def create_items(items: OrderItem):
     # Open a connection
@@ -85,7 +122,7 @@ def create_items(items: OrderItem):
         session.refresh(items)
         return items
 
-
+# Read items from an order
 @app.get("/items")
 def read_items():
     with Session(engine) as session:
